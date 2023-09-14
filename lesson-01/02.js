2fb6b74810965552044162c9b842927558800545
Ivan Mironovich <ivan.mironovich97@yandex.ru>
2023-09-14 18:06:26 +0500

    feat: lesson 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,7 +1,13 @@
 // code
 let number1 = Math.floor(Math.random() * 100)
-if(number1 % 2 === 0) {
-    console.log('Это четное число');
-} else {
-    console.log('это не четное число')
+
+function randomNumbers (number) {
+    if(number % 2 === 0) {
+        console.log('Это четное число');
+    } else {
+        console.log('это не четное число')
+    }
 }
+
+randomNumbers(number1)
+
