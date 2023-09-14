4c23a931be0440b197b7dd16c2b9140f9e11611d
Ivan Mironovich <ivan.mironovich97@yandex.ru>
2023-09-14 18:25:38 +0500

    feat: шаблонные литералы

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -3,9 +3,9 @@ let number1 = Math.floor(Math.random() * 100)
 
 function randomNumbers (number) {
     if(number % 2 === 0) {
-        console.log('Это четное число');
+        console.log(`${number} :Это четное число`);
     } else {
-        console.log('это не четное число')
+        console.log(`${number} :это не четное число`)
     }
 }
 
